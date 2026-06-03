b3aaeb943f35dfbbcbdb4810c5dbb7904d9faf21
skill craft technology <adarshbr33@gmail.com>
2026-06-03 21:21:00 +0530

    Completed Week 1 Day 3

---
+++ Week 1/Day 3/SquaresofaSortedArray.java
@@ -0,0 +1,25 @@
+public class SquaresofaSortedArray {
+    class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int []ans=new int[nums.length];
+        int start=0;
+        int end=nums.length-1;
+        int ptr=nums.length-1;
+        while(start<=end){
+            int stsq=nums[start]*nums[start];//start ka square
+            int endsq=nums[end]*nums[end];//end ka square
+            if(stsq>endsq){
+                ans[ptr]=stsq;
+                start++;
+            }
+            else{
+                ans[ptr]=endsq;
+                end--;          
+                  }
+                  ptr--;
+        }
+        return ans;
+    }
+}
+}
+
